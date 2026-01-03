bc07c3aaa7e6f4a1d14aa073a2a2f4d0b72b75f6
NicolasOmar <nicolas.passerino@gmail.com>
2026-01-03 13:04:04 +0100

    chore: probable fixing on server side rendering with an orm

--- app/medicine/page.tsx
+++ app/medicine/page.tsx
@@ -1,13 +1,10 @@
 // CORE
-import { FC } from 'react'
 import { prisma } from '@prisma/index'
 // COMPONENTS
 import MedicineForm from '@form-components/MedicineForm'
 
-export const MedicinePage: FC = async () => {
+export default async function MedicinePage() {
   const listOfData = await prisma.medicinePresentation.findMany()
 
   return <MedicineForm listOfPresentations={listOfData ?? []} />
 }
-
-export default MedicinePage
